a2fafe356f0b6d98daa77fde2efb19e5efa9150d
Lichapa <lichapau@gmail.com>
2021-12-08 15:03:08 +0200

    Added Home and Quote Component, Added route for every page

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -27,146 +27,155 @@ function Calculator() {
   };
 
   return (
-    <div className="main-container" id="main-container">
+    <div className="main-container flex row center">
       <div>
-        <input
-          type="number"
-          className="display-calc grey"
-          id="display-Calc"
-          readOnly
-          value={display()}
-        />
+        <h2>Let&apos;s do some math</h2>
       </div>
-      <div className="buttons">
-        <button
-          type="button"
-          className="white AC"
-          onClick={() => handleClick('AC')}
-        >
-          AC
-        </button>
-        <button
-          type="button"
-          className="white plus-minus"
-          onClick={() => handleClick('+/-')}
-        >
-          +/-
-        </button>
-        <button
-          type="button"
-          className="white percent"
-          onClick={() => handleClick('%')}
-        >
-          %
-        </button>
-        <button
-          type="button"
-          className="orange divide"
-          onClick={() => handleClick('÷')}
-        >
-          ÷
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('7')}
-        >
-          7
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('8')}
-        >
-          8
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('9')}
-        >
-          9
-        </button>
-        <button
-          type="button"
-          className="orange multiply"
-          onClick={() => handleClick('x')}
-        >
-          x
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('4')}
-        >
-          4
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('5')}
-        >
-          5
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('6')}
-        >
-          6
-        </button>
-        <button
-          type="button"
-          className="orange subtract"
-          onClick={() => handleClick('-')}
-        >
-          -
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('1')}
-        >
-          1
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('2')}
-        >
-          2
-        </button>
-        <button
-          type="button"
-          className="white number-btn"
-          onClick={() => handleClick('3')}
-        >
-          3
-        </button>
-        <button
-          type="button"
-          className="orange addition"
-          onClick={() => handleClick('+')}
-        >
-          +
-        </button>
-        <button type="button" className="zero" onClick={() => handleClick('0')}>
-          0
-        </button>
-        <button
-          type="button"
-          className="white decimal"
-          onClick={() => handleClick('.')}
-        >
-          .
-        </button>
-        <button
-          type="button"
-          className="orange equal"
-          onClick={() => handleClick('=')}
-        >
-          =
-        </button>
+      <div className="calculator" id="main-container">
+        <div>
+          <input
+            type="number"
+            className="display-calc grey"
+            id="display-Calc"
+            readOnly
+            value={display()}
+          />
+        </div>
+        <div className="buttons">
+          <button
+            type="button"
+            className="white AC"
+            onClick={() => handleClick('AC')}
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            className="white plus-minus"
+            onClick={() => handleClick('+/-')}
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            className="white percent"
+            onClick={() => handleClick('%')}
+          >
+            %
+          </button>
+          <button
+            type="button"
+            className="orange divide"
+            onClick={() => handleClick('÷')}
+          >
+            ÷
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('7')}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('8')}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('9')}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            className="orange multiply"
+            onClick={() => handleClick('x')}
+          >
+            x
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('4')}
+          >
+            4
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('5')}
+          >
+            5
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('6')}
+          >
+            6
+          </button>
+          <button
+            type="button"
+            className="orange subtract"
+            onClick={() => handleClick('-')}
+          >
+            -
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('1')}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('2')}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            className="white number-btn"
+            onClick={() => handleClick('3')}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            className="orange addition"
+            onClick={() => handleClick('+')}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="zero"
+            onClick={() => handleClick('0')}
+          >
+            0
+          </button>
+          <button
+            type="button"
+            className="white decimal"
+            onClick={() => handleClick('.')}
+          >
+            .
+          </button>
+          <button
+            type="button"
+            className="orange equal"
+            onClick={() => handleClick('=')}
+          >
+            =
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eum
+        labore sequi. Qui dolorum enim deserunt minima ratione, consequuntur
+        quas esse culpa aliquam rem, accusantium consectetur labore odio
+        repellat consequatur?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eum
+        labore sequi. Qui dolorum enim deserunt minima ratione, consequuntur
+        quas esse culpa aliquam rem, accusantium consectetur labore odio
+        repellat consequatur?
+      </p>
+    </div>
+  );
+}
+export default Home;
